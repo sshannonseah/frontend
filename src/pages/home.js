c151a9b6aa5f947c4140d96a55886ee576c3a0aa
sshannonseah <23018646@myrp.edu.sg>
2025-01-22 20:36:33 +0800

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,7 +13,7 @@ const Home = () => {
 
     useEffect(()=> {
         const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts')
+            const response = await fetch('/api/workout')
             const json = await response.json()
 
             if (response.ok) {
